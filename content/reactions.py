219c2f5aa0ef39187687fdc64ac3409961a9771e
Gabriel P. S <gabrpereira@icloud.com>
2025-06-07 19:37:08 -0300

    Moved reactions to another directory

---
+++ content/reactions.py
@@ -0,0 +1,17 @@
+reactions = {
+        "Reação 1: Ácido clorídrico + Hidróxido de sódio": {
+            "equation": "HCl + NaOH -> NaCl + H2O",
+            "reactants": {"HCl": 1, "NaOH": 1},
+            "products": {"NaCl": 1, "H2O": 1}
+            },
+        "Reação 2: Óxido de cálcio + Água": {
+            "equation": "CaO + H2O -> Ca(OH)2",
+            "reactants": {"CaO": 1, "H2O": 1},
+            "products": {"Ca(OH)2": 1}
+            },
+        "Reação 3: Dióxido de carbono + Água": {
+            "equation": "CO2 + H2O -> H2CO3",
+            "reactants": {"CO2": 1, "H2O": 1},
+            "products": {"H2CO3": 1}
+            }
+        }
