a26927fb4e658c9286c89cf1b5a1a7956db2960e
Gabriel P. S <gabrpereira@icloud.com>
2025-06-19 21:40:26 -0300

    Fixed organic function

--- content/reactions.py
+++ content/reactions.py
@@ -13,10 +13,38 @@ reactions = {
             "equation": "CO2 + H2O -> H2CO3",
             "reactants": {"CO2": 1, "H2O": 1},
             "products": {"H2CO3": 1}
-            }
-        # "Reação 4: ...": {    TODO Exemplo de como adicionar uma nova reação
-        #     "equation": "...",
-        #     "reactants": {"...": 1, "...": 1},
-        #     "products": {"...": 1}
-        # }
-        }
+            },
+        "Reação 4: Magnésio + Oxigênio": {
+            "equation": "2Mg + O2 -> 2MgO",
+            "reactants": {"Mg": 2, "O2": 1},
+            "products": {"MgO": 2}
+        },
+        "Reação 5: Hidrogênio + Cloro": {
+            "equation": "H2 + Cl2 -> 2HCl",
+            "reactants": {"H2": 1, "Cl2": 1},
+            "products": {"HCl": 2}
+        },
+        "Reação 6: Zinco + Ácido clorídrico": {
+            "equation": "Zn + 2HCl -> ZnCl2 + H2",
+            "reactants": {"Zn": 1, "HCl": 2},
+            "products": {"ZnCl2": 1, "H2": 1}
+        },
+    "Reação 7: Ácido sulfúrico + Hidróxido de sódio": {
+            "equation": "H2SO4 + 2NaOH -> Na2SO4 + 2H2O",
+            "reactants": {"H2SO4": 1, "NaOH": 2},
+            "products": {"Na2SO4": 1, "H2O": 2}
+        },
+        "Reação 8: Nitrato de prata + Cloreto de sódio": {
+            "equation": "AgNO3 + NaCl -> AgCl + NaNO3",
+            "reactants": {"AgNO3": 1, "NaCl": 1},
+            "products": {"AgCl": 1, "NaNO3": 1}
+        },
+        "Reação 9: Metano + Oxigênio": {
+            "equation": "CH4 + 2O2 -> CO2 + 2H2O",
+            "reactants": {"CH4": 1, "O2": 2},
+            "products": {"CO2": 1, "H2O": 2}
+        },
+        "Reação 10: Óxido de cobre(II) + Ácido sulfúrico": {
+            "equation": "CuO + H2SO4 -> CuSO4 + H2O",
+            "reactants": {"CuO": 1, "H2SO4": 1},
+            "products": {"CuSO4": 1, "H2O": 1}}}
